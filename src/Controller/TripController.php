5666de6b8a870eb0889ac6d58ebee161053a2754
Yan2802 <126497197+Yan2802@users.noreply.github.com>
2023-04-28 18:16:45 +0200

    Update TripController.php

--- src/Controller/TripController.php
+++ src/Controller/TripController.php
@@ -2,39 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\City;
+use App\Entity\Place;
 use APP\Entity\Trip;
+use App\Form\PlaceType;
 use App\Form\TripFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/trip', name: 'trip_')]
 class TripController extends AbstractController
 {
-    #[Route('/trip/create', name: 'app_create')]
-     public function create(Request $request, EntityManagerInterface $entityManager): Response
-     {
-     $trip = new Trip();
-     $form = $this->createForm(TripFormType::class, $trip);
 
-     $form->handleRequest($request);
-     if ($form->isSubmitted() && $form->isValid()) {
+    #[Route('/create', name: 'create')]
+    public function create(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $trip = new Trip();
 
-         //$request->request->getClicked();
+        $form = $this->createForm(TripFormType::class, $trip);
+        $form->handleRequest($request);
 
-         $trip = $form->getData();
-         //$trip = $TripRepository->trip($form);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($trip);
+            $entityManager->flush();
 
-         $entityManager->persist($trip);
-         $entityManager->flush();
+            return $this->redirectToRoute('app_main');
+        }
 
-         return $this->redirectToRoute('app_main');
-     }
-
-     return $this->render('trip/create.html.twig', [
-         'TripForm' => $form->createView(),
-     ]);
+        return $this->render('trip/create.html.twig', [
+            'TripForm' => $form->createView(),
+        ]);
     }
 }
 
