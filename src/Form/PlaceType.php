d2a8c8bd81e2b22679de18d307c728cf7a8587d8
Yan2802 <126497197+Yan2802@users.noreply.github.com>
2023-04-28 18:17:35 +0200

    Create PlaceType.php

---
+++ src/Form/PlaceType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\City;
+use App\Entity\Place;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlaceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('city', EntityType::class, [
+                'label' => 'Ville : ',
+                'class' => City::class,
+                'choice_label' => 'name',
+            ])
+            ->add('name', TextType::class, [
+                'label' => 'Lieu : ',
+                ])
+            ->add('street',TextType::class,['label' => 'Rue : '])
+            ->add('postalCode', EntityType::class, [
+                'label' => 'Code postal : ',
+                'class' => City::class,
+                'choice_label' => 'postalCode',
+            ])
+            ->add('latitude', TextType::class, ['label' => 'Latitude : '])
+            ->add('longitude', TextType::class, ['label' => 'Longitude : '])
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Place::class,
+        ]);
+    }
+}
