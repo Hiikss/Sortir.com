99d0b433c328b1957b46e311203b1c41e91bdb45
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-05-02 18:57:21 +0200

    add disable users

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,8 +15,6 @@ class UserController extends AbstractController
     #[Route('/admin/user/list', name: 'admin_user_list')]
     public function list(UserRepository $userRepository, Request $request): Response
     {
-        
-
         $form = $this->createFormBuilder()
             ->add('searchzone', TextType::class, [
                 'required' => false,
@@ -38,5 +37,20 @@ class UserController extends AbstractController
             'users' => $users,
             'form' => $form
         ]);
+    }
+
+    #[Route('/admin/user/active/{id}', name: 'admin_user_active')]
+    public function active(int $id, UserRepository $userRepository, EntityManagerInterface $em): Response
+    {
+        $user = $userRepository->find($id);
+
+        if($user) {
+           $user->setActive(!$user->isActive());
+
+           $em->persist($user);
+           $em->flush();
+        }
+
+        return $this->redirectToRoute('admin_user_list');
     }
 }
