7943551d5861df54f7f16ce8c127dde366cffa09
Thomas <91778476+Hiikss@users.noreply.github.com>
2023-05-03 16:08:44 +0200

    add campus modifier

--- src/Controller/CampusController.php
+++ src/Controller/CampusController.php
@@ -85,4 +85,32 @@ class CampusController extends AbstractController
             'form' => $form
         ]);
     }
+
+    #[Route('/modify/{id}', name: 'modify')]
+    public function modify(int $id, CampusRepository $campusRepository, Request $request, EntityManagerInterface $em): Response
+    {
+        $campus = $campusRepository->find($id);
+
+        $campusForm = $this->createForm(CampusType::class, $campus);
+
+        $campusForm->handleRequest($request);
+
+        if ($campusForm->isSubmitted() && $campusForm->isValid()) {
+            $campus= $campusForm->getData();
+
+            $em->persist($campus);
+            $em->flush();
+
+            $this->addFlash(
+                'success',
+                'Le campus a bien été modifié !'
+            );
+
+            return $this->redirectToRoute('admin_campus_list');
+        }
+
+        return $this->render('campus/modify.html.twig', [
+            'campusForm' => $campusForm
+        ]);
+    }
 }
